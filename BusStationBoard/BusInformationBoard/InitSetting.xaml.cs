ec61a05a88fe059b7bf09f105cde7d52a79f4b39
sonseunghyeong <seunghyeongson@gmail.com>
2024-02-07 09:27:24 +0900

    first upload

---
+++ BusStationBoard/BusInformationBoard/InitSetting.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BusInformationBoard
+{
+    /// <summary>
+    /// InitSetting.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class InitSetting : Window
+    {
+        public InitSetting()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_INIT_CONFIRM_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void BTN_INIT_CANCEL_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ BusStationBoard/BusInformationBoard/Setting/Setting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusInformationBoard.Setting
+{
+    public class Setting
+    {
+        public static string CONST_STR_SETTING_FILE_PATH = "./config.json";
+
+
+        public string serverInfo = "";
+
+        public void load() {
+        }
+
+        public void save() { 
+        }
+    }
+}
